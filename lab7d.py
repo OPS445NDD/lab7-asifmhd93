b40031d31cdb9bee123228146f2569ccb436f796
Asif Mohammad <asifmhd93@gmail.com>
2026-07-10 17:35:54 -0400

    Added lab7d

--- lab7d.py
+++ lab7d.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
-# Student ID: [seneca_id] 
+# Student ID: amohammad21
+
+#!/usr/bin/env python3
+# Student ID: amohammad21
+
 class Time:
     """Simple object type for time of the day.
-        data attributes: hour, minute, second
-        function attributes: __init__, __str__, __repr__
+       data attributes: hour, minute, second
+       function attributes: __init__, __str__, __repr__
                             time_to_sec, format_time,
                             change_time, sum_time
     """
     def __init__(self,hour=12,minute=0,second=0):
-        """constructor for time object""" 
+        """constructor for time object"""
         self.hour = hour
         self.minute = minute
         self.second = second
@@ -19,19 +23,18 @@ class Time:
 
     def sum_times(self, t2):
         """Add two time objests and return the sum."""
-        self_sec = self.time_to_sec()
-        t2_sec = t2.time_to_sec()
-        sum = sec_to_time(self_sec + t2_sec)
+        time_seconds = self.time_to_sec() + t2.time_to_sec()
+        sum = sec_to_time(time_seconds)
         return sum
 
     def change_time(self, seconds):
         time_seconds = self.time_to_sec()
         nt = sec_to_time(time_seconds + seconds)
-        self.hour, self.minute, self.second = nt.hour, nt.minute, nt.second 
+        self.hour, self.minute, self.second = nt.hour, nt.minute, nt.second
         return None
 
     def time_to_sec(self):
-        '''convert a time object to a single integer representing the 
+        '''convert a time object to a single integer representing the
         number of seconds from mid-night'''
         minutes = self.hour * 60 + self.minute
         seconds = minutes * 60 + self.second
@@ -41,14 +44,14 @@ class Time:
         """check for the validity of the time object attributes:
         24 > hour > 0, 60 > minute > 0, 60 > second > 0 """
         if self.hour < 0 or self.minute < 0 or self.second < 0:
-            return False
+           return False
         if self.minute >= 60 or self.second >= 60 or self.hour >= 24:
-            return False
+           return False
         return True
 
 def sec_to_time(seconds):
-    '''convert a given number of seconds to a time object in 
-        hour, minute, second format'''
+    '''convert a given number of seconds to a time object in
+       hour, minute, second format'''
     time = Time()
     minutes, time.second = divmod(seconds, 60)
     time.hour, time.minute = divmod(minutes, 60)
