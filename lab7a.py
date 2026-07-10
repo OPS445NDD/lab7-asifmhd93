7cf9638cf9391917e79a7c1ef28d6e113a6d7335
Asif Mohammad <asifmhd93@gmail.com>
2026-07-10 17:34:59 -0400

    Added lab7a

--- lab7a.py
+++ lab7a.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
-# Student ID: [seneca_id]
+# Student ID: amohammad21
+
+
 class Time:
-    """Simple object type for time of the day.
-    data attributes: hour, minute, second
-    """
-    def __init__(self,hour=12,minute=0,second=0):
-        """constructor for time object""" 
-        self.hour = hour
-        self.minute = minute
-        self.second = second
+   """Simple object type for time of the day.
+      data attributes: hour, minute, second
+   """
+   def __init__(self,hour=12,minute=0,second=0):
+       """constructor for time object"""
+       self.hour = hour
+       self.minute = minute
+       self.second = second
 
 def format_time(t):
     """Return time object (t) as a formatted string"""
@@ -20,13 +22,22 @@ def sum_times(t1, t2):
     sum.hour = t1.hour + t2.hour
     sum.minute = t1.minute + t2.minute
     sum.second = t1.second + t2.second
+
+    while sum.second >= 60:
+        sum.second = sum.second - 60
+        sum.minute = sum.minute + 1
+
+    while sum.minute >= 60:
+        sum.minute = sum.minute - 60
+        sum.hour = sum.hour + 1
+
     return sum
 
 def valid_time(t):
     """check for the validity of the time object attributes:
-        24 > hour > 0, 60 > minute > 0, 60 > second > 0 """
+       24 > hour > 0, 60 > minute > 0, 60 > second > 0 """
     if t.hour < 0 or t.minute < 0 or t.second < 0:
-        return False
+       return False
     if t.minute >= 60 or t.second >= 60 or t.hour >= 24:
-        return False
+       return False
     return True
